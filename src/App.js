0e24b12b38941e6044a65eac2b6318231e0ef90b
Payne <paynezh@yahoo.ca>
2018-04-08 01:22:41 -0400

    initial

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+//import './../styles/app.css';
+import './styles/appStyles.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+        <h1>Title goes here</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
+// return (
+//   <div className="App">
+//     <header className="App-header">
+//       <img src={logo} className="App-logo" alt="logo" />
+//       <h1 className="App-title">Welcome to React</h1>
+//     </header>
+//     <p className="App-intro">
+//       To get started, edit <code>src/App.js</code> and save to reload.
+//     </p>
+//   </div>
+// );
